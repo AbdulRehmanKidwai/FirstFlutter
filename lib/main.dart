9f7d7c3940d632944e18729e1a96b4766205a983
abdul <abdulkidwai1994@gmail.com>
2024-11-27 18:08:23 +0530

    Day 4 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_flutter/screens/home_page.dart';
 import 'package:first_flutter/screens/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
+      theme: ThemeData(fontFamily: GoogleFonts.lato().fontFamily),
       initialRoute: "/login",
       routes: {
         "/": (context) => LoginPage(),
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -3,13 +3,42 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-          "Login",
-          style: TextStyle(
-              fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold),
-        ),
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          const SizedBox(height: 100.0),
+          Image.asset("assets/images/login_image.png", fit: BoxFit.cover),
+          const SizedBox(height: 20.0),
+          const Text(
+            "Welcome",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: "Enter username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      hintText: "Enter password", labelText: "Password"),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+              style: const ButtonStyle(
+                  foregroundColor: WidgetStatePropertyAll(Colors.white),
+                  backgroundColor: WidgetStatePropertyAll(Colors.deepPurple)),
+              onPressed: () {},
+              child: const Text("Login"))
+        ],
       ),
     );
   }
